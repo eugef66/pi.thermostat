27521f864ba4b9ccc45a5383823d20395da3016d
eugef66 <eugef66@gmail.com>
2019-05-20 18:08:51 -0400

    changed template to Jinja2 template

--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-from bottle import route, run, template
+from bottle import route, run, template, jinja2_template as template
 import thermostat as th
 
 @route('/')
@@ -6,5 +6,9 @@ def index():
 	t=th.thermostat()
 	ct,ch = t.Current_Temperature_Humidity
 	return template('index_template', Status=t.Status, Current_Temperature=ct, Current_Humidity=ch, Color='blue',Target_Temperature=t.Target_Temperature, Current_Mode=t.Mode)
+
+def set(mode, temp):
+	t=th.thermostat()
+	t.Set(temp,mode)
 
 run(host='localhost', port=8888)
