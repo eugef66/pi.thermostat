742e40c1223b9041d9ac8c7467dc68ed13b0a227
eugef66 <eugef66@gmail.com>
2019-05-22 16:32:25 -0400

    changes to session managment

--- web.py
+++ web.py
@@ -1,38 +1,22 @@
 from bottle import route, run, template, jinja2_template as template, static_file, redirect, request
-import hashlib
-from  datetime import datetime, date
 import thermostat as th
+from auth import auth
 
-def checkLogin():
-	# Update to handle miltiple browsers
-	session_timeout = 20
-	valid=False
-	session_age= session_timeout+1
-	try:
-		sf = open (".session","r")
-		session_date=datetime.strptime(sf.read(), '%Y-%m-%d %H:%M:%S.%f')
-		sf.close()
-		if session_date:
-			session_age=((datetime.now()-session_date).seconds/60)
-	except IOError:
-		pass
-	print (session_age)
-	if session_age<session_timeout:
-		return True
-	else:
-		redirect('/login')
+
+a=auth()
+a.redirect_url="/login"
 
 
 @route('/')
 def index():
-	if checkLogin()==True:
+	if a.is_logged_in:
 		t=th.thermostat()
 		ct,ch = t.Current_Temperature_Humidity
 		return template('index_template', Status=t.Status, Current_Temperature=ct, Current_Humidity=ch, Color='blue',Target_Temperature=t.Target_Temperature, Current_Mode=t.Mode)
 
 @route('/set/<mode>/<temp>')
 def set(mode, temp):
-	if checkLogin()==True:
+	if a.is_logged_in:
 		t=th.thermostat()
 		t.Set(temp,mode)
 		return redirect('/');
@@ -44,27 +28,12 @@ def login():
 @route('/login', method='POST')
 def do_login():
 	pin = request.forms.get('pin')
-	pin_hash = hashlib.sha256(pin.encode()).hexdigest()
-	#print (pin_hash)
-	if pin_hash == '1b8ba0b107410f67c70ab6ed4abf4e0ec0e70df78298e41d1670c7c1e94a703f':
-		start_session()
+	if a.login(pin):
 		redirect ('/')
 	else: 
-		# destroy session
 		return template('login_template', Message='Login incorrect')
 
-def start_session():
-	#Update to handle multiple browsers
-	session_file = open(".session","w+t")
-	session_file.write(str(datetime.now()))
-	session_file.close()
-	return
 
-def destroy_session():
-	session_file = open(".session","w+t")
-	session_file.write("")
-	session_file.close()
-	return
 
 # static files
 @route('/images/<filename>')
