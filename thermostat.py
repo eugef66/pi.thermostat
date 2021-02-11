55b5202b132207c1b38c0f499b3eaa12f9b42ac4
Eugene <efurer@gmail.com>
2021-02-11 13:14:21 -0500

    started adding logic for issue #3

--- thermostat.py
+++ thermostat.py
@@ -23,57 +23,19 @@ class thermostat:
 
 	__db = {}
 
-	__HEAT_Pin = 2
-	__HEAT2_Pin = 17
-	__COOL_Pin = 3
-	__Sensor_Pin = 4
-
-	# Config parammeters
-	__Calib = 0
-	__Immed_action = False
-
 	def __init__(self, **kwargs):
 		# Setup GPIO
 		GPIO.setmode(GPIO.BCM)
 		GPIO.setwarnings(False)
-		GPIO.setup([self.__HEAT_Pin, self.__HEAT2_Pin,
-					self.__COOL_Pin], GPIO.OUT)
-		# Read config file
-		self.__Calib = config.CALIBRATION
-		self.__Immed_action = config.PERFORM_IMIDIATE_ACTION
-		self.__HEAT_Pin = config.HEAT_PIN
-		self.__HEAT2_Pin = config.HEAT_STAGE_2_PIN
-		self.__COOL_Pin = config.COOL_PIN
-		self.__Sensor_Pin = config.SENSOR_PIN
 
-		# config_file = open(self.__ConfigFile,"r")
-		# for line in config_file:
-		#    if line:
-		#        kv = line.split("=")
-		#        if kv[0] == "CALIBRATION":
-		#            self.__Calib = int(kv[1].strip())
-		#            continue
-		#        if kv[0] == "PERFORM_IMIDIATE_ACTION":
-		#            self.__Immed_action = (kv[1].strip()=="True")
-		#            continue
-		# print self.__Calib
-		# print self.__Immed_action
+		GPIO.setup([config.HEAT_PIN, config.COOL_PIN], GPIO.OUT)
 
-		# Read mode file
+		if (config.HEAT_TWO_STAGE):
+			GPIO.setup([config.HEAT_STAGE_2_PIN], GPIO.OUT)
+		if (config.COOL_TWO_STAGE):
+			GPIO.setup([config.COOL_STAGE_2_PIN], GPIO.OUT)
+
 		self.__load_db()
-		
-		
-
-		# mode_file = open(self.__ModeFile, "r")
-		# for line in mode_file:
-		#    if line:
-		#        kv = line.split("=")
-		#        if kv[0] == "TargetTemperature":
-		#            self.__target_temperature = int(kv[1].strip())
-		#            continue
-		#        if kv[0] == "Mode":
-		#            self.__mode = kv[1].strip()
-		#            continue
 
 		return
 
@@ -87,25 +49,25 @@ class thermostat:
 	@property
 	def Current_Temperature(self):
 		sensor = Adafruit_DHT.DHT22
-		humidity, temp = Adafruit_DHT.read_retry(sensor, self.__Sensor_Pin)
-		temp = round(temp * 9/5.0 + 32, 2)+self.__Calib
+		humidity, temp = Adafruit_DHT.read_retry(sensor, config.SENSOR_PIN)
+		temp = round(temp * 9/5.0 + 32, 2) + config.CALIBRATION
 		return temp
 
 	@property
 	def Current_Temperature_Humidity(self):
 		sensor = Adafruit_DHT.DHT22
-		humidity, temp = Adafruit_DHT.read_retry(sensor, self.__Sensor_Pin)
-		temp = round(temp * 9/5.0 + 32, 2)+self.__Calib
+		humidity, temp = Adafruit_DHT.read_retry(sensor, config.SENSOR_PIN)
+		temp = round(temp * 9/5.0 + 32, 2) + config.CALIBRATION
 		humidity = round(humidity, 2)
 		return temp, humidity
 
 	@property
 	def Status(self):
-		if GPIO.input(self.__HEAT2_Pin) == GPIO.LOW:
+		if GPIO.input(config.HEAT_STAGE_2_PIN) == GPIO.LOW:
 			return "HEAT", "2"
-		elif GPIO.input(self.__HEAT_Pin) == GPIO.LOW:
+		elif GPIO.input(config.HEAT_PIN) == GPIO.LOW:
 			return "HEAT", "1"
-		elif GPIO.input(self.__COOL_Pin) == GPIO.LOW:
+		elif GPIO.input(config.COOL_PIN) == GPIO.LOW:
 			return "COOL", ""
 		else:
 			return "OFF", ""
@@ -120,7 +82,7 @@ class thermostat:
 		self.__db["target_temperature"]=int(targetTemp)
 		self.__db["schedule"]=schedule
 		self.__save_db()
-		if self.__Immed_action or Mode == "OFF":
+		if config.PERFORM_IMIDIATE_ACTION or Mode == "OFF":
 			self.Process()
 
 	def Process(self):
@@ -130,26 +92,33 @@ class thermostat:
 		schedule = self.__db["schedule"]
 		schedule = datetime.strptime(schedule, '%Y-%m-%d %H:%M')
 		
+		#TODO: The logiv below is wrong (issue #3)
+		# Modify process to take into consideration 2 stage pins may not be set
+
 		# Perfrom process only if current date and time aftre scheduled one
 		if (datetime.now() > schedule):
-			if target_temperature - temp > 3 and (mode == "HEAT" or mode == "AUTO"):
+			if config.HEAT_TWO_STAGE and target_temperature - temp > 3 and (mode == "HEAT" or mode == "AUTO"):
 				# Set HEAT_2=ON, COOL=OFF
-				GPIO.output([self.__HEAT_Pin, self.__HEAT2_Pin, self.__COOL_Pin],
+				GPIO.output([config.HEAT_PIN, config.HEAT_STAGE_2_PIN, config.COOL_PIN],
 							(GPIO.LOW, GPIO.LOW, GPIO.HIGH))
 				return "ON"
 			elif target_temperature - temp > 1 and (mode == "HEAT" or mode == "AUTO"):
 				# Set HEAT_1=ON, COOL=OFF
-				GPIO.output([self.__HEAT_Pin, self.__HEAT2_Pin, self.__COOL_Pin],
+				GPIO.output([config.HEAT_PIN, config.HEAT_STAGE_2_PIN, config.COOL_PIN],
 							(GPIO.LOW, GPIO.HIGH, GPIO.HIGH))
 				return "ON"
-			elif temp - target_temperature > 1 and (mode == "COOL" or mode == "AUTO"):
-				# Process Cool Logic
+			elif config.COOL_TWO_STAGE and temp - target_temperature > 3 and (mode == "COOL" or mode == "AUTO"):
 				# Set HEAT=OFF, COOL=ON"
-				GPIO.output([self.__HEAT_Pin, self.__HEAT2_Pin, self.__COOL_Pin],
+				GPIO.output([config.HEAT_PIN, config.HEAT_STAGE_2_PIN, config.COOL_PIN],
+							(GPIO.HIGH, GPIO.HIGH, GPIO.LOW))
+				return "ON"
+			elif temp - target_temperature > 1 and (mode == "COOL" or mode == "AUTO"):
+				# Set HEAT=OFF, COOL=ON"
+				GPIO.output([config.HEAT_PIN, config.HEAT_STAGE_2_PIN, config.COOL_PIN],
 							(GPIO.HIGH, GPIO.HIGH, GPIO.LOW))
 				return "ON"
 			else:
-				GPIO.output([self.__HEAT_Pin, self.__HEAT2_Pin, self.__COOL_Pin],
+				GPIO.output([config.HEAT_PIN, config.HEAT_STAGE_2_PIN, config.COOL_PIN],
 							(GPIO.HIGH, GPIO.HIGH, GPIO.HIGH))
 				return "OFF"
 			GPIO.cleanup()
